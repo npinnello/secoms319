f5be0002588b8ffb10cfda9db1b4de44f70dd34f
npinnello <npin@iastate.edu>
2023-04-24 11:40:53 -0500

    new

--- node_web_server/Activity12/npin_Activity12_app.js
+++ node_web_server/Activity12/npin_Activity12_app.js
@@ -3,6 +3,19 @@ var cors = require("cors");
 var app = express();
 var fs = require("fs");
 var bodyParser = require("body-parser");
+const { MongoClient } = require("mongodb");
+
+
+// Mongo
+const url ="mongodb://127.0.0.1:27017";
+const dbName ="reactdata";
+const client = new MongoClient(url);
+const db = client.db(dbName);
+
+
+
+
+
 
 app.use(cors());
 app.use(bodyParser.json());
