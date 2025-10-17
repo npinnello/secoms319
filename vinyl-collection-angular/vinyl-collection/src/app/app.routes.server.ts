cb366b1a0267392b20bceddceb697d8fc30ef081
Nick Pinnello <npinnello1234@gmail.com>
2025-10-17 16:05:13 -0500

    small fix

--- vinyl-collection-angular/vinyl-collection/src/app/app.routes.server.ts
+++ vinyl-collection-angular/vinyl-collection/src/app/app.routes.server.ts
@@ -2,6 +2,27 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'genre/:genre',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      // Return the genre parameters that should be prerendered
+      return [
+        { genre: 'rap' },
+        { genre: 'rock' },
+        { genre: 'pop' },
+        { genre: 'soundtracks' }
+      ];
+    }
+  },
+  {
+    path: 'album/:id',
+    renderMode: RenderMode.Server // Use server rendering for album details since there are many albums
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
